d8da3dea2f96845114617ab698bf009bd6f983d9
lucianoleyriaa <lucholeyria@gmail.com>
2023-05-12 12:01:40 +0200

    Add login, icons and images

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html',
+})
+export class BtnComponent implements OnInit {
+
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() type: 'success' | 'danger' | 'warning' = 'success';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
+    // './pages/**/*.{html,js}',
+    // './components/**/*.{html,js}',
+    // './components/btn/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: '#37b24d',
+        primary:'#1971c2',
+        danger: '#e03131'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
