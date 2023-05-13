e1beb01eef96cd747a7c92c02956832780a01ace
lucianoleyriaa <lucholeyria@gmail.com>
2023-05-13 15:49:19 +0200

    Finish drag and drop

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../models/task';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -9,16 +8,15 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 })
 export class BoardComponent implements OnInit {
 
-    tasks: Task[] = [
-        { id: '1', name: 'Refactorizar' },
-        { id: '1', name: 'Crear' },
-        { id: '1', name: 'Deployar' },
-        { id: '1', name: 'Testear' },
-    ]
-
     todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
     doing = ['Get up', 'Brush teeth', 'Check e-mail', 'Walk dog'];
     done = ['Take a shower', 'Take out the garbage', 'Do the laundry'];
+
+    columns = [
+        { name: 'To Do', items: this.todo },
+        { name: 'Doing', items: this.doing },
+        { name: 'Done', items: this.done },
+    ];
 
     constructor() { }
 
@@ -28,6 +26,10 @@ export class BoardComponent implements OnInit {
     drop(e: CdkDragDrop<string[]>) {
         if (e.container === e.previousContainer) return moveItemInArray(e.container.data, e.previousIndex, e.currentIndex);
         transferArrayItem(e.previousContainer.data, e.container.data, e.previousIndex, e.currentIndex);
+    }
+
+    onAddColumn() {
+        this.columns.push({ name: 'New Column', items: [] });
     }
 
 }
